c0d0e3222f4e95f9fb827c565ab6e1a241faf659
zdis1125 <zdis1125@gmail.com>
2026-01-21 22:04:06 -0500

    Update Scraper.R

--- Scraper.R
+++ Scraper.R
@@ -1,16 +1,21 @@
 library(rvest)
 library(dplyr)
 library(httr)
-library(polite)
+
 
 # Define the URL and the User Agent
 url <- "https://kenpom.com/"
 
 
+my_ua <- Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36
+url <- "https://kenpom.com/"
 
+# Use GET with your User Agent
+response <- GET(url, user_agent(my_ua))
 
-session <- bow("https://kenpom.com/", user_agent = "zdis1125@gmail.com")
-page <- scrape(session)
+# Now read the HTML
+page <- read_html(response)
+
 
 Ranks <- page %>%
   html_nodes(".hard_left") %>%
